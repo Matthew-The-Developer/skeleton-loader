1bd0366454eecabcd291a6c10eac34bef78445a6
Matthew Watson <Me@MatthewTheDeveloper.com>
2023-01-17 09:14:48 -0500

    - created basic layout - started cards - installed charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    NgxSkeletonLoaderModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
